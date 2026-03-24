db636764893c96b795e63e4c910494e58fbb787d
piyapat <piyapat.thongrueng@gmail.com>
2026-03-24 09:56:33 +0700

    first commit

---
+++ 01-MyInfo/MyInfo.java
@@ -0,0 +1,11 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        String name = "John Doe";
+        int age = 20;
+        String hobby = "Playing guitar";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
+    }
+}
